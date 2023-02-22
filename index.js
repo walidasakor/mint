bffec191858a4575a5950dd462031b65a9371b8a
Osgood <osgoodackom@gmail.com>
2023-02-22 00:59:51 +0000

    worked on the localstorage aspect

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const output = document.getElementById("output")
+const oz = document.getElementById("oz")
+const descript = document.getElementById("descript")
+const buyNow = document.querySelector(".buy_now")
 
-function buyNow(itemDetails) {
-    const itemsInCart = JSON.parse?.(localStorage.getItem("itemsInCart")) || [];
-    itemsInCart.push(itemDetails);
-    localStorage.setItem("itemsInCart", JSON.stringify(itemsInCart));
-    output.innerHTML = localStorage.getItem("itemsInCart")
-  }
-  
-  function viewCart() {
-    const itemsInCart = JSON.parse?.(localStorage.getItem("itemsInCart")) || [];
-    console.log(itemsInCart);
-  }
 
-  function clearAll(){
-    localStorage.removeItem("itemsInCart");
-
-  }
+buyNow.addEventListener("click", function(){
+  descript.innerText = 'thus is the price'
+})
