0dc6050ab59257126a20b81e69e77c35709142c3
Osgood <osgoodackom@gmail.com>
2023-02-23 01:05:08 +0000

    updated js

--- index.js
+++
@@ -1,8 +0,0 @@
-const oz = document.getElementById("oz")
-const descript = document.getElementById("descript")
-const buyNow = document.querySelector(".buy_now")
-
-
-buyNow.addEventListener("click", function(){
-  descript.innerText = 'thus is the price'
-})
